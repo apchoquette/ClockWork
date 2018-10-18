406ebdf2dc8080f56e8b8d63e0ffce61dee18450
Andy Choquette <work@Andys-MacBook-Pro.local>
2018-10-18 11:14:33 -0700

    authentication

---
+++ server/services/Passport.js
@@ -0,0 +1,14 @@
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
+
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.clientSecret,
+    callbackURL: "http://localhost:3000/api/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
